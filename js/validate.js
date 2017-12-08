1be4714573854dbe4c10e9470e61db0ef992ab82
zippn <nguyenhuuvu@gmail.com>
2017-12-08 14:37:14 -0500

    Final

--- js/validate.js
+++ js/validate.js
@@ -1,4 +1,6 @@
+//Setting vars
 let falseCount = 0;
+//Get inputs
 const name = $('#name');
 const mail = $('#mail');
 const activities = $('.activities input');
@@ -6,30 +8,74 @@ const ccNum = $('#cc-num');
 const zipNum = $('#zip');
 const cvvNum = $('#cvv');
 
+//input check on the go
+form.addEventListener('input',function (e) {
+    //console.log(e.target);
+    var input = $(e.target);
+    var inputName = input.attr('id');
+    //console.log(input.attr('id'));
 
-mail.on('input',function () {
-    if(!validateEmail(mail.val())) {
-        addError(mail);
-    }else {
-        $(this).prev().removeClass('required-input');
+    removeError(input);
 
-        $(this).removeClass('required-input');
+    switch (inputName){
 
+        case 'name':
+
+            if ($.trim(input.val()).length===0){
+
+                addError(input);
+            }
+            break;
+        case 'mail':
+            if (!validateEmail(input.val())){
+                addError(input);
+            }
+            break;
+        case 'cc-num':
+            if (!checkCCNum(input.val())) {
+                addError(input);
+
+            }
+            break;
+        case 'zip':
+            if (!checkZip(input.val())) {
+                addError(input);
+
+            }
+            break;
+        case 'cvv':
+            if (!checkCVV(input.val())) {
+                addError(input);
+
+            }
+            break;
     }
-});
-function validate(e) {
 
+
+});
+
+function removeError(e) {
+  //  console.log(e);
+    e.prev().removeClass('required-input empty-input');
+
+    e.removeClass('required-input empty-input');
+
+
+
+}
+//validate form
+function validate(e) {
+    //set/reset validation checks
     let validForm = false;
     falseCount = 0;
 
-
     $('.required-input').each(function () {
-        console.log(this);
-       $(this).removeClass('required-input');
+       // console.log(this);
+       $(this).removeClass('required-input empty-input');
 
     });
-
-    if (!name.val()){
+    //check inputs
+    if ($.trim(name.val()).length===0){
         addError(name);
     }
     if(!validateEmail(mail.val())) {
@@ -39,6 +85,7 @@ function validate(e) {
         addError($('.activities'));
 
     }
+    //check credit card if selected
      if (selectPayment.selectedIndex===1) {
          if (!checkCCNum(ccNum.val())) {
              addError(ccNum);
@@ -51,26 +98,40 @@ function validate(e) {
              addError(cvvNum);
          }
 
-         if (falseCount === 0) {
-             validForm = true;
-         }
 
-         if (!validForm) {
-             e.preventDefault();
-             window.location.assign("#top");
-
-         }
      }
+    //valid form
+    if (falseCount === 0) {
+        validForm = true;
+    }
+    //invalid form
+    if (!validForm) {
+        e.preventDefault();
+        window.location.assign("#top");
+
+    }
     
 }
+//add error warning for invalid iput
 function addError(selector) {
     falseCount +=1;
-    console.log($('[for="'+selector.attr('id')+'"]'));
+    console.log('err');
+    //console.log($('[for="'+selector.attr('id')+'"]'));
     $('[for="'+selector.attr('id')+'"]').addClass('required-input');
     selector.addClass('required-input');
 
+    if(selector.attr('type')!=='checkbox'){
+        console.log(selector.attr('type'));
+        if(!selector.val()||selector.val()===" "){
+            $('[for="'+selector.attr('id')+'"]').addClass('empty-input');
+            selector.addClass('empty-input');
+
+        }
+    }
+
 
 }
+//check e-mail
 function validateEmail(sEmail) {
     var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
     if (filter.test(sEmail)) {
@@ -79,6 +140,7 @@ function validateEmail(sEmail) {
         return false;
     }
 }
+//check activity
 function checkActivity(selector) {
     let activityCount = false;
 
@@ -91,6 +153,7 @@ function checkActivity(selector) {
     return activityCount;
 
 }
+//check credit card
 function checkCCNum(ccNum) {
     //console.log(ccNum.length);
     if (ccNum.length>=13&&ccNum.length<=16){
@@ -100,6 +163,7 @@ function checkCCNum(ccNum) {
     }
 
 }
+//check zip code
 function checkZip(zip) {
    // console.log(zip.length);
     if (zip.length===5){
@@ -109,8 +173,9 @@ function checkZip(zip) {
     }
 
 }
+//check CVV
 function checkCVV(cvv) {
-    console.log(cvv.length);
+    //console.log(cvv.length);
     if (cvv.length===3){
         return true;
     }else {
